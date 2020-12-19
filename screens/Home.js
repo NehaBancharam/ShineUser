3f02a37543570d9c68dee614d141741fbdfdb24a
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-19 17:11:57 +0400

    added categories card

--- screens/Home.js
+++ screens/Home.js
@@ -1,50 +1,105 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+
+const { width, height } = Dimensions.get("screen");
 
 export default Home = () => {
+  const themeBackground = require("../assets/home.png");
   return (
-    <View style={styles.container}>
-      {/* header style */}
-      <View style={styles.header}>
-        <Text style={{ fontWeight: "500", fontSize: 20 }}>Hi Neha!</Text>
-      </View>
-      <View>
-        <Image
-          style={styles.img}
-          source={require("../assets/home.png")}
-        ></Image>
-        <View style={styles.imgText}>
-          <Text style={{ marginTop: -120, color: "#ffffff", fontSize: 30 }}>
-            Monthly THEME 
-          </Text>
+    <ScrollView scrollEnabled>
+      <View style={styles.container}>
+        {/* header style */}
+        <View style={styles.header}>
+          <Text style={{ fontWeight: "500", fontSize: 20 }}>Hi Neha!</Text>
+        </View>
+        <View style={{ flex: 1, padding: 15 }}>
+          <ImageBackground
+            style={styles.img}
+            source={themeBackground}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <Text
+              style={{
+                fontFamily: "ibarra-italic",
+                color: "#ffffff",
+                fontSize: 30,
+                textAlign: "center",
+              }}
+            >
+              Monthly
+            </Text>
+          </ImageBackground>
+        </View>
+        <View
+          style={{ flex: 2, backgroundColor: "white", marginHorizontal: 15 }}
+        >
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              paddingVertical: 15,
+            }}
+          >
+            <Text>Tasks To Complete</Text>
+          </View>
+          <View>
+            <Text style={{ paddingVertical: 15 }}>Self Care</Text>
+            <View
+              style={{
+                width: width / 4,
+                height: 100,
+                backgroundColor: "black",
+                borderRadius: 20,
+              }}
+            >
+              <View style={{ flex: 3, backgroundColor: "blue" }}></View>
+              <View
+                style={{
+                  flex: 2,
+                  backgroundColor: "red",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text>Task Name</Text>
+              </View>
+            </View>
+          </View>
         </View>
       </View>
-      <View style={{margin: 10}}>
-        <Text>Self care</Text>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height,
+    paddingTop: 25,
+    backgroundColor: "white",
   },
   header: {
     flexDirection: "row",
-    marginTop: 30,
-    justifyContent: "flex-start",
-    marginLeft: -10,
-    paddingHorizontal: 32,
-    paddingVertical: 12,
+    padding: 15,
     borderBottomWidth: 0.5,
     borderBottomColor: "#D8D9DB",
   },
   imgText: {
     flexDirection: "row",
-    justifyContent:"center",
+    justifyContent: "center",
   },
   img: {
-    height: 200,
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    elevation: 5,
   },
 });
