a007d0d91d8f75cf55e7f674339ff2e5663513b1
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-19 20:58:36 +0400

    added styling

--- screens/Post.js
+++ screens/Post.js
@@ -8,10 +8,14 @@ import {
   Image,
   TextInput,
   ActivityIndicator,
+  Dimensions,
 } from "react-native";
 import firebase from "../config/Firebase";
 
 import { MaterialIcons } from "@expo/vector-icons";
+import { ScrollView } from "react-native-gesture-handler";
+
+const { height } = Dimensions.get("screen");
 
 export default Post = ({ navigation }) => {
   const [text, setText] = useState("");
@@ -23,75 +27,88 @@ export default Post = ({ navigation }) => {
     setLoading(true);
     let postRef = firebase.firestore().collection("posts").doc();
 
-    let post = {
-      uid: userID,
-      text,
-      timestamp: firebase.firestore.Timestamp.now(),
-    };
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(userID)
+      .get()
+      .then((doc) => {
+        let post = {
+          uid: userID,
+          username: doc.data().username,
+          text,
+          timestamp: firebase.firestore.Timestamp.now(),
+        };
 
-    postRef.set(post).then(() => {
-      firebase
-        .firestore()
-        .collection("users")
-        .doc(userID)
-        .update({
-          posts: firebase.firestore.FieldValue.arrayUnion(postRef.id),
-        })
-        .then(() => {
-          setLoading(false);
-          navigation.goBack();
+        postRef.set(post).then(() => {
+          firebase
+            .firestore()
+            .collection("users")
+            .doc(userID)
+            .update({
+              posts: firebase.firestore.FieldValue.arrayUnion(postRef.id),
+            })
+            .then(() => {
+              setLoading(false);
+              navigation.goBack();
+            });
         });
-    });
+      });
   };
 
-  useEffect(() => {
-    console.log(userID);
-  }, []);
-
   return (
-    <View style={styles.container}>
-      {/* header style */}
-      <View style={styles.header}>
-        <Text style={{ fontWeight: "500", fontSize: 20 }}>Post </Text>
-      </View>
-      {loading ? (
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <ActivityIndicator size="large" color="black" />
+    <ScrollView scrollEnabled>
+      <View style={styles.container}>
+        {/* header style */}
+        <View style={styles.header}>
+          <Text style={{ fontWeight: "500", fontSize: 20 }}>Post </Text>
         </View>
-      ) : (
-        <View style={{ flex: 1 }}>
+        {loading ? (
           <View
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
           >
-            <Image
-              source={require("../assets/logo/logo.png")}
-              style={styles.avatar}
-            />
+            <ActivityIndicator size="large" color="black" />
           </View>
+        ) : (
+          <View style={{ flex: 1 }}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                source={require("../assets/logo/logo.png")}
+                style={styles.avatar}
+              />
+            </View>
 
-          <View style={styles.inputContainer}>
-            <TextInput
-              autoFocus={true}
-              multiline={true}
-              numberOfLines={4}
-              // textAlign="center"
-              placeholder="Share your journey with other Shine Brightly members!"
-              onChangeText={(text) => setText(text)}
-              value={text}
-            ></TextInput>
-            <TouchableOpacity style={styles.photo} onPress={postSubmitHandler}>
-              <MaterialIcons
-                name="check-circle"
-                size={40}
-                color="#634C87"
-              ></MaterialIcons>
-            </TouchableOpacity>
+            <View style={styles.inputContainer}>
+              <TextInput
+                autoFocus={true}
+                multiline={true}
+                numberOfLines={4}
+                // textAlign="center"
+                placeholder="Share your journey with other Shine Brightly members!"
+                onChangeText={(text) => setText(text)}
+                value={text}
+              ></TextInput>
+              <TouchableOpacity
+                style={styles.photo}
+                onPress={postSubmitHandler}
+              >
+                <MaterialIcons
+                  name="check-circle"
+                  size={40}
+                  color="#634C87"
+                ></MaterialIcons>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-      )}
-    </View>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -100,6 +117,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+    height,
   },
   header: {
     flexDirection: "row",
@@ -109,15 +127,15 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     borderBottomWidth: 1,
     borderBottomColor: "#D8D9DB",
+    backgroundColor: "white",
   },
   inputContainer: {
-    flex: 1,
+    flex: 2,
     alignItems: "center",
   },
   avatar: {
-    width: "70%",
-    height: "70%",
-    marginRight: 16,
+    width: 200,
+    height: 200,
   },
   photo: {
     flexDirection: "row",
