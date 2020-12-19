625404285cef276a946c4e064e481ad9f19cf9a9
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-19 20:58:13 +0400

    added all and user posts

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,97 +1,212 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
-import { Avatar, FAB } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  RefreshControl,
+} from "react-native";
+import { Avatar, FAB, Searchbar } from "react-native-paper";
 
 import { FontAwesome5, FontAwesome } from "@expo/vector-icons";
 
 import firebase from "../config/Firebase";
-import { useEffect } from "react";
+
+import { LogBox } from "react-native";
+import _ from "lodash";
+import PostCard from "../components/feed/PostCard";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const { width, height } = Dimensions.get("screen");
 const logo = require("../assets/logo/logo.png");
 
+LogBox.ignoreLogs(["Setting a timer"]);
+const _console = _.clone(console);
+console.warn = (message) => {
+  if (message.indexOf("Setting a timer") <= -1) {
+    _console.warn(message);
+  }
+};
+
+const Tab = createMaterialTopTabNavigator();
+
 export default Feed = ({ navigation }) => {
   const [posts, setPosts] = useState([]);
+  const [userPosts, setUserPosts] = useState([]);
 
   const [loading, setLoading] = useState(false);
+  const [showAll, setShowAll] = useState(true);
 
-  const getPostsHandler = () => {
+  const userID = firebase.auth().currentUser.uid;
+
+  const getPostsHandler = (query) => {
     setLoading(true);
 
+    if (query) {
+      firebase
+        .firestore()
+        .collection("posts")
+        .where("username", "==", query)
+        .get()
+        .then((querySnapshot) => {
+          let tempPosts = [];
+          querySnapshot.forEach(function (postDoc) {
+            tempPosts.push({
+              id: postDoc.id,
+              ...postDoc.data(),
+            });
+          });
+          setPosts(tempPosts);
+          setLoading(false);
+        })
+        .catch((error) => {});
+    } else {
+      firebase
+        .firestore()
+        .collection("posts")
+        .orderBy("timestamp", "desc")
+        .limit(10)
+        .get()
+        .then((querySnapshot) => {
+          let tempPosts = [];
+          querySnapshot.forEach(function (postDoc) {
+            tempPosts.push({
+              id: postDoc.id,
+              ...postDoc.data(),
+            });
+          });
+          setPosts(tempPosts);
+          setLoading(false);
+        })
+        .catch((error) => {});
+    }
+  };
+
+  const getUserPostsHandler = () => {
+    setLoading(true);
     firebase
       .firestore()
       .collection("posts")
-      .orderBy("timestamp", "desc")
-      .limit(10)
+      .where("uid", "==", userID)
       .get()
-      .then((doc) => {
+      .then((querySnapshot) => {
+        let tempPosts = [];
+        querySnapshot.forEach(function (postDoc) {
+          tempPosts.push({
+            id: postDoc.id,
+            ...postDoc.data(),
+          });
+        });
+        setUserPosts(tempPosts);
         setLoading(false);
-        setPosts(doc.data());
-        console.log(doc.data());
       })
       .catch((error) => {});
   };
 
   useEffect(() => {
-    const subscriber = getPostsHandler();
-    return subscriber;
+    getPostsHandler();
+    getUserPostsHandler();
   }, []);
 
   return (
     <View style={styles.container}>
-      {/* header style */}
       <View style={styles.header}>
         <Text style={{ fontWeight: "500", fontSize: 20 }}>Feed </Text>
       </View>
-      <View style={{ flex: 1 }}>
+      <View style={{ flexDirection: "row" }}>
         <View
           style={{
-            margin: 15,
-            elevation: 3,
-            backgroundColor: "white",
+            flex: 1,
             padding: 15,
+            justifyContent: "center",
+            alignItems: "center",
+            borderBottomWidth: showAll ? 1 : 0,
+            borderBottomColor: "purple",
           }}
         >
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              paddingBottom: 15,
+          <TouchableOpacity
+            onPress={() => {
+              if (showAll) {
+                return;
+              } else {
+                setShowAll(!showAll);
+              }
             }}
           >
-            <FontAwesome name="user-circle-o" size={30} color="gray" />
-            <View style={{ marginHorizontal: 10 }}>
-              <Text style={{ color: "gray", fontSize: 17, fontWeight: "bold" }}>
-                Username
-              </Text>
-              <Text style={{ color: "gray", fontSize: 14 }}>TimeStamp</Text>
-            </View>
-          </View>
-
-          <View
-            style={{
-              width: width - 60,
-            }}
-          >
-            <Text style={{ textAlign: "justify" }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-              vehicula, nisl quis aliquam iaculis, leo odio rutrum lacus, et
-              pharetra elit ipsum id est. Mauris aliquam lectus ac tortor
-              auctor, vitae semper massa posuere. Morbi accumsan egestas
-              facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit. Nulla sit amet erat rutrum, lacinia erat at, interdum
-              ligula. Praesent eu arcu efficitur, sagittis orci a, consectetur
-              nunc. Aenean aliquam justo ac congue feugiat. Duis at nisl eros.
-              Sed tempus maximus velit, sed placerat enim facilisis eu. Nunc non
-              faucibus tortor, ut porta odio. Aliquam viverra ut quam vitae
-              rhoncus. Duis sagittis eu dolor sit amet vehicula. Aenean interdum
-              eros nec leo placerat, eget suscipit mi porttitor. Fusce nec quam
-              vel velit lobortis ornare vel at purus. Donec ac condimentum arcu,
-              in consectetur nunc. Maecenas luctus elit eu nisl lacinia, in
-              posuere purus mollis.
-            </Text>
-          </View>
+            <Text>All Posts</Text>
+          </TouchableOpacity>
         </View>
+        <View
+          style={{
+            flex: 1,
+            padding: 15,
+            justifyContent: "center",
+            alignItems: "center",
+            borderBottomWidth: showAll ? 0 : 1,
+            borderBottomColor: "purple",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              if (showAll) {
+                setShowAll(!showAll);
+              } else {
+                return;
+              }
+            }}
+          >
+            <Text>Your Posts</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        {showAll ? (
+          <>
+            <Searchbar
+              placeholder="Search By Username"
+              onChangeText={(search) => getPostsHandler(search)}
+            />
+
+            <View style={{ flex: 1, marginTop: 5 }}>
+              <FlatList
+                data={posts}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => <PostCard post={item} />}
+                refreshControl={
+                  <RefreshControl
+                    colors={["black"]}
+                    progressBackgroundColor="white"
+                    refreshing={loading}
+                    onRefresh={() => {
+                      getPostsHandler();
+                    }}
+                  />
+                }
+              />
+            </View>
+          </>
+        ) : (
+          <View style={{ flex: 1, marginTop: 5 }}>
+            <FlatList
+              data={userPosts}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => <PostCard post={item} />}
+              refreshControl={
+                <RefreshControl
+                  colors={["black"]}
+                  progressBackgroundColor="white"
+                  refreshing={loading}
+                  onRefresh={() => {
+                    getUserPostsHandler();
+                  }}
+                />
+              }
+            />
+          </View>
+        )}
       </View>
       <FAB
         style={styles.fab}
@@ -105,15 +220,16 @@ export default Feed = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "white",
   },
   header: {
     flexDirection: "row",
-    marginTop: 20,
     justifyContent: "center",
     paddingHorizontal: 32,
     paddingVertical: 12,
     borderBottomWidth: 1,
     borderBottomColor: "#D8D9DB",
+    backgroundColor: "white",
   },
   fab: {
     position: "absolute",
