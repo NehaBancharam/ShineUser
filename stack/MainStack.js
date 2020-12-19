9e3eb92d76da0d25e3fd64da964d21b2f83b199c
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-19 17:09:47 +0400

    added isloggedin check

--- stack/MainStack.js
+++ stack/MainStack.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AuthStack from "./AuthStack";
 import AppStack from "./AppStack";
 
+import firebase from "../config/Firebase";
+
 export default MainStack = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const MainStack = createStackNavigator();
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
 
   return (
     <MainStack.Navigator headerMode="none">
-      {/* <MainStack.Screen name="Auth" component={AuthStack} /> */}
-      <MainStack.Screen name="App" component={AppStack} />
+      {isLoggedIn ? (
+        <MainStack.Screen name="App" component={AppStack} />
+      ) : (
+        <MainStack.Screen name="Auth" component={AuthStack} />
+      )}
     </MainStack.Navigator>
   );
 };
