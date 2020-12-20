4729621f1d622b47a19e6e379e055604058db8c8
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-20 22:25:26 +0400

    added forgot password screen

---
+++ screens/Authentication/ForgotPassword.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ForgotPassword = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>ForgotPassword</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default ForgotPassword;
