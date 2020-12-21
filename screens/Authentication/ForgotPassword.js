d2ae7aaac346e5dcabb57942b34d168ccbb0e665
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-21 18:18:33 +0400

    added forgot password screen and logic

--- screens/Authentication/ForgotPassword.js
+++ screens/Authentication/ForgotPassword.js
@@ -1,16 +1,207 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import { TextInput } from "react-native-paper";
+import firebase from "../../config/Firebase";
 
-const ForgotPassword = (props) => {
+import { MaterialIcons } from "@expo/vector-icons";
+
+const { height } = Dimensions.get("screen");
+
+const logo = require("../../assets/login.png");
+
+const ForgotPassword = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [emailInvalid, setEmailInvalid] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [reset, setReset] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  const clearErrors = () => {
+    setErrorMessage("");
+    setEmailInvalid(false);
+  };
+
+  const resetPasswordHandler = () => {
+    clearErrors();
+
+    setLoading(true);
+
+    if (email.length === 0) {
+      setErrorMessage("Enter your email.");
+      setEmailInvalid(true);
+      setLoading(false);
+    } else {
+      firebase
+        .auth()
+        .sendPasswordResetEmail(email)
+        .then(() => {
+          setLoading(false);
+          setReset(true);
+        })
+        .catch((error) => {
+          errorMessageHandler(error.code);
+          console.log(error.code);
+        });
+    }
+  };
+
+  const errorMessageHandler = (code) => {
+    if (code === "auth/too-many-requests") {
+      setErrorMessage("Too many requests. Try again later.");
+      setLoading(false);
+    } else if (code === "auth/user-not-found") {
+      setErrorMessage("User not found");
+      setEmailInvalid(true);
+      setLoading(false);
+    } else if (code === "auth/invalid-email") {
+      setErrorMessage("Invalid email");
+      setEmailInvalid(true);
+      setLoading(false);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>ForgotPassword</Text>
-    </View>
+    <ScrollView scrollEnabled>
+      <View style={styles.container}>
+        <View
+          style={{
+            marginTop: 45,
+            marginBottom: 15,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={logo}
+            resizeMode="contain"
+            style={{ width: "60%" }}
+          ></Image>
+        </View>
+        <View style={styles.msg}>
+          <Text style={{ textAlign: "center", fontWeight: "bold" }}>
+            An email will be sent to you on the email address provided. You can
+            change your password from there.
+          </Text>
+        </View>
+
+        {errorMessage ? (
+          <View style={styles.msg}>
+            <Text style={styles.error}>{errorMessage}</Text>
+          </View>
+        ) : (
+          <></>
+        )}
+
+        {/* //form code */}
+        <View style={styles.form}>
+          <TextInput
+            onFocus={clearErrors}
+            error={emailInvalid}
+            label="Email Address"
+            mode="outlined"
+            style={styles.input}
+            onChangeText={(email) => setEmail(email.trim())}
+            value={email.trim()}
+            keyboardType="email-address"
+            theme={{ colors: { primary: "purple" } }}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={{
+            ...styles.button,
+            backgroundColor: reset ? "#00bf33" : "#E9446A",
+          }}
+          onPress={
+            reset ? () => navigation.navigate("Login") : resetPasswordHandler
+          }
+        >
+          {loading ? (
+            <ActivityIndicator size="large" color="white" />
+          ) : reset ? (
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Text
+                style={{ color: "#FFF", fontWeight: "bold", marginRight: 5 }}
+              >
+                Sign In
+              </Text>
+              <MaterialIcons name="check" size={24} color="white" />
+            </View>
+          ) : (
+            <Text style={{ color: "#FFF", fontWeight: "bold" }}>
+              Reset Password
+            </Text>
+          )}
+        </TouchableOpacity>
+
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            paddingVertical: 25,
+          }}
+        >
+          <Text style={{ color: "#414959", fontSize: 13 }}>
+            Want to sign in instead?{" "}
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <Text style={{ fontSize: 13, fontWeight: "500", color: "#E9446A" }}>
+              Sign In here
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    height,
+    backgroundColor: "white",
+  },
+  form: {
+    marginVertical: 15,
+    marginHorizontal: 30,
+  },
+  msg: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 30,
+    marginVertical: 15,
+  },
+  error: {
+    color: "#E9446A",
+    fontSize: 13,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  input: {
+    fontSize: 15,
+    color: "#161F3D",
+    backgroundColor: "white",
+    marginVertical: 5,
+  },
+  button: {
+    marginHorizontal: 30,
+    borderRadius: 4,
+    height: 52,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default ForgotPassword;
