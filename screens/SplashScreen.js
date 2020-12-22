c0f3e558ca36779e6d8248d0bd226252f0a1cb06
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-22 13:04:30 +0400

    added splashscreen component

---
+++ screens/SplashScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const splashScreen = require("../assets/splash-screen.png");
+const SplashScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1, margin: 15, borderWidth: 1 }}>
+        <Image
+          source={splashScreen}
+          resizeMode="center"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#300016",
+  },
+});
+
+export default SplashScreen;
