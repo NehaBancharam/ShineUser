63264fcd01785b233813e2b2bc5fbd2ea06ce544
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-22 13:05:30 +0400

    added onboarding logic check

--- stack/AuthStack.js
+++ stack/AuthStack.js
@@ -1,17 +1,54 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect, useState } from "react";
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from "@react-navigation/stack";
 import Login from "../screens/Authentication/Login";
 import SignUp from "../screens/Authentication/SignUp";
 import ForgotPassword from "../screens/Authentication/ForgotPassword";
+import OnBoarding from "../screens/Authentication/OnBoarding";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import SplashScreen from "../screens/SplashScreen";
 
 export default AuthStack = () => {
   const AuthStack = createStackNavigator();
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem("alreadyLaunched").then((value) => {
+      if (value === null) {
+        AsyncStorage.setItem("alreadyLaunched", "true");
+        setIsFirstLaunch(true);
+        console.log("first launch");
+      } else {
+        setIsFirstLaunch(false);
+        console.log("NOT first launch");
+      }
+    });
+  }, []);
 
   return (
-    <AuthStack.Navigator headerMode="none">
-      <AuthStack.Screen name="Login" component={Login} />
-      <AuthStack.Screen name="SignUp" component={SignUp} />
-      <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
+    <AuthStack.Navigator
+      headerMode="none"
+      screenOptions={{
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}
+    >
+      {isFirstLaunch === null ? (
+        <AuthStack.Screen name="SplashScreen" component={SplashScreen} />
+      ) : (
+        <>
+          {isFirstLaunch === true ? (
+            <AuthStack.Screen name="OnBoarding" component={OnBoarding} />
+          ) : (
+            <></>
+          )}
+          <AuthStack.Screen name="Login" component={Login} />
+          <AuthStack.Screen name="SignUp" component={SignUp} />
+          <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
+        </>
+      )}
     </AuthStack.Navigator>
   );
 };
