3295a67af44d306c130818e8c5b522a48980c109
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-26 09:55:24 +0400

    added check network connection

--- stack/MainStack.js
+++ stack/MainStack.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import NetInfo from "@react-native-community/netinfo";
 import AuthStack from "./AuthStack";
 import AppStack from "./AppStack/AppStack";
 
 import firebase from "../config/Firebase";
 import SplashScreen from "../screens/SplashScreen";
+import { Alert } from "react-native";
+import NoInternet from "../screens/NoInternet";
 
 export default MainStack = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [isConnected, setIsConnected] = useState(null);
 
   const MainStack = createStackNavigator();
 
@@ -23,14 +27,31 @@ export default MainStack = () => {
     return subscriber;
   }, []);
 
+  useEffect(() => {
+    const subscriber = NetInfo.addEventListener((state) => {
+      if (state.isConnected) {
+        setIsConnected(true);
+      } else {
+        setIsConnected(false);
+      }
+    });
+
+    // To unsubscribe to these update, just use:
+    return subscriber;
+  });
+
   return (
     <MainStack.Navigator headerMode="none">
       {isLoggedIn === null ? (
         <MainStack.Screen name="SplashScreen" component={SplashScreen} />
-      ) : isLoggedIn === true ? (
-        <MainStack.Screen name="App" component={AppStack} />
+      ) : isConnected ? (
+        isLoggedIn === true ? (
+          <MainStack.Screen name="App" component={AppStack} />
+        ) : (
+          <MainStack.Screen name="Auth" component={AuthStack} />
+        )
       ) : (
-        <MainStack.Screen name="Auth" component={AuthStack} />
+        <MainStack.Screen name="NoInternet" component={NoInternet} />
       )}
     </MainStack.Navigator>
   );
