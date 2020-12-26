27bdecbcdca68514f4fcca84aeb63247eadcd7f5
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-26 10:00:55 +0400

    changed styling

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-
-import { MaterialIcons } from "@expo/vector-icons";
-
-const Header = ({ title, left, right }) => {
+import { View, Text, StyleSheet } from "react-native";
+const Header = ({ title, left, right, style }) => {
   return (
-    <View style={styles.header}>
+    <View style={{ ...styles.header, ...style }}>
       <View style={{ flex: 1 }}>{left}</View>
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ fontWeight: "500", fontSize: 20 }}>{title}</Text>
+        <Text style={{ fontWeight: "500", fontSize: 18 }}>{title}</Text>
       </View>
       <View style={{ flex: 1, flexDirection: "row-reverse" }}>{right}</View>
     </View>
@@ -20,7 +17,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     paddingHorizontal: 15,
-    paddingVertical: 15,
+    paddingBottom: 15,
+    paddingTop: 30,
     borderBottomWidth: 1,
     borderBottomColor: "#D8D9DB",
     backgroundColor: "white",
