4a4a56c1f85d9faf96b7b489ed9181855410488b
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-29 12:52:39 +0400

    changed styling

--- components/home/CategoryCard.js
+++ components/home/CategoryCard.js
@@ -14,50 +14,62 @@ const { width, height } = Dimensions.get("screen");
 
 const CategoryCard = ({ navigation, item, title }) => {
   return (
-    <View
-      style={{
-        margin: 10,
-        padding: 10,
-        elevation: 3,
-        backgroundColor: "white",
-        width: width / 2,
-        justifyContent: "center",
-      }}
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("ViewCategory", {
+          title,
+          ...item,
+        })
+      }
     >
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate("ViewCategory", {
-            title,
-            ...item,
-          })
-        }
+      <View
+        style={{
+          margin: 10,
+          padding: 10,
+          elevation: 3,
+          backgroundColor: "white",
+          width: 150,
+          height: 150,
+          justifyContent: "center",
+        }}
       >
-        {item.completed ? (
-          <View
-            style={{
-              width: "100%",
-              height: "100%",
-              position: "absolute",
-              top: 0,
-              right: 0,
-              backgroundColor: "rgba(0, 191, 51, 0.05)",
-              zIndex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <MaterialIcons name="check-circle" size={50} color="green" />
-          </View>
-        ) : (
-          <></>
-        )}
-        <Image
-          source={{ uri: item.imageUrl }}
-          style={{ width: "100%", height: 100 }}
-        />
-        <Text style={{ textAlign: "center" }}>{item.name}</Text>
-      </TouchableOpacity>
-    </View>
+        <View style={{ flex: 4 }}>
+          {item.completed ? (
+            <View
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: 0,
+                right: 0,
+                backgroundColor: "rgba(0, 191, 51, 0.05)",
+                zIndex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <MaterialIcons name="check-circle" size={50} color="green" />
+            </View>
+          ) : (
+            <></>
+          )}
+          <Image
+            source={{ uri: item.image.imageUrl }}
+            resizeMode="cover"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ textAlign: "center" }}>{item.name}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
